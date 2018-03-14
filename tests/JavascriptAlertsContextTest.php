e6a579c0d1bb11d888510c8ee136890aeedb04f2
Tadas <tadas@edmondscommerce.co.uk>
2018-03-14 17:21:10 +0000

    Initialized JavascriptAlertsContext test class

---
+++ tests/JavascriptAlertsContextTest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\BehatJavascriptContext;
+
+
+use Behat\Mink\Mink;
+use EdmondsCommerce\MockServer\MockServer;
+
+class JavascriptAlertsContextTest extends AbstractTestCase
+{
+    /**
+     * @var JavascriptAlertsContext
+     */
+    private $context;
+
+    /**
+     * @var MockServer
+     */
+    private $server;
+
+    /**
+     * @throws \Exception
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        //Set up the mock server
+        $this->server = new MockServer(__DIR__ . '/assets/routers/router.php', $this->getContainerIp(), 8080);
+        $this->server->startServer();
+
+        $mink = new Mink(['selenium2' => $this->seleniumSession]);
+        $mink->setDefaultSessionName('selenium2');
+        $this->seleniumSession->start();
+
+        //Set up Mink in the class
+        $this->context = new JavascriptAlertsContext();
+        $this->context->setMink($mink);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+        $this->seleniumSession->stop();
+        $this->server->stopServer();
+    }
+}
