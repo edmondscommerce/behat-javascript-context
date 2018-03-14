8981cf96c44b87bc36e615f7d39c7efc13d4cd3b
Tadas <tadas@edmondscommerce.co.uk>
2018-03-14 16:53:00 +0000

    Initialized AbstractTestCase

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+namespace EdmondsCommerce\BehatJavascriptContext;
+
+use Behat\Mink\Driver\Selenium2Driver;
+use PHPUnit\Framework\TestCase;
+use Behat\Mink\Session;
+
+abstract class AbstractTestCase extends TestCase
+{
+    /**
+     * @var Session
+     */
+    protected $seleniumSession;
+
+    protected $containerIp;
+
+    /**
+     * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
+     * @param bool $headless
+     */
+    protected function setUpSeleniumMink(bool $headless = true)
+    {
+        $args = [
+            'args' => [
+                '--disable-gpu',
+                '--window-size=1920,1080',
+                '--start-maximised',
+            ],
+        ];
+
+        if ($headless) {
+            $args['args'][] = '--headless';
+        }
+
+        $driver = new Selenium2Driver('chrome', $args);
+
+        $this->seleniumSession = new Session($driver);
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->setUpSeleniumMink();
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     * @return mixed
+     */
+    protected function getContainerIp()
+    {
+        $commandToExecute = 'ip addr show eth0 | grep "inet\b" | awk \'{print $2}\' | cut -d/ -f1';
+
+        exec($commandToExecute, $commandOutput, $exitCode);
+
+
+        return array_pop($commandOutput);
+    }
+}
