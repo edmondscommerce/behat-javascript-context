c5b4c66a8369f5720b857a5d45d6db7165c56c07
Tadas <tadas@edmondscommerce.co.uk>
2018-03-21 11:09:55 +0000

    Enabled chrome headless

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -43,7 +43,7 @@ abstract class AbstractTestCase extends TestCase
     public function setUp()
     {
         parent::setUp();
-        $this->setUpSeleniumMink(false);
+        $this->setUpSeleniumMink();
     }
 
     /**
